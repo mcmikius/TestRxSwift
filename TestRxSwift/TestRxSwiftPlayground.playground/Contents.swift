adc300a7c4f4a245490ef93a9e64ba4a7636b848
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-11-22 15:47:41 +0200

    fix

---
+++ TestRxSwift/TestRxSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+#if NOT_IN_PLAYGROUND
+    
+    public func playgroundShouldContinueIndefinitely() { }
+    
+#else
+    
+    import PlaygroundSupport
+    
+    public func playgroundShouldContinueIndefinitely() {
+        PlaygroundPage.current.needsIndefiniteExecution = true
+    }
+    
+#endif
