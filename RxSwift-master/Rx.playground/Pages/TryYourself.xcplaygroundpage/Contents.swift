105f3dc390f70457b8f540ed530fdf213bf26d07
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-11-27 17:56:53 +0200

    Lesson two https://www.youtube.com/watch?v=NaHI4-FkvNI&list=PLtXyatp5fW31MyvQK7x4B4bBNYzXIpgMJ&index=2

--- RxSwift-master/Rx.playground/Pages/TryYourself.xcplaygroundpage/Contents.swift
+++ RxSwift-master/Rx.playground/Pages/TryYourself.xcplaygroundpage/Contents.swift
@@ -13,6 +13,8 @@ import RxSwift
  */
 playgroundShouldContinueIndefinitely()
 
+// First Lessons
+ 
 example("Try yourself") {
   // let disposeBag = DisposeBag()
   _ = Observable.just("Hello, RxSwift!")
@@ -113,4 +115,85 @@ example("merge") {
     mergeSequence.subscribe { (event) in
         print(event)
     }
+}
+
+
+// Second Lessons
+
+
+example("PublishSubject") {
+    let disposableBag = DisposeBag()
+    let subject = PublishSubject<String>()
+    subject.subscribe {
+        print("Subscription First: ", $0)
+    }.disposed(by: disposableBag)
+    
+    enum MyError: Error {
+        case Text
+    }
+    
+    subject.on(Event<String>.next("Hello"))
+//    subject.onCompleted()
+//    subject.onError(MyError.Text)
+    subject.onNext("RxSwift")
+    subject.subscribe(onNext: {
+        print("Subscription Second: ", $0)
+    }).disposed(by: disposableBag)
+    subject.onNext("Hi")
+    subject.onNext("Swift")
+}
+
+example("BehaviorSubject") {
+    let disposableBag = DisposeBag()
+    let subject = BehaviorSubject(value: 1)
+    
+    let firstSubscription = subject.subscribe(onNext: {
+        print(#line, $0)
+        }).disposed(by: disposableBag)
+    subject.onNext(2)
+    subject.onNext(3)
+    let secondSubscrioption = subject.map({ $0 + 2 }).subscribe(onNext: {
+        print(#line, $0)
+    }).disposed(by: disposableBag)
+}
+
+example("ReplaySubject") {
+    let disposableBag = DisposeBag()
+//    let subject = ReplaySubject<String>.create(bufferSize: 1)
+//
+//    subject.subscribe {
+//        print("Subscription First: ", $0)
+//    }.disposed(by: disposableBag)
+//
+//    subject.onNext("a")
+//    subject.onNext("b")
+//
+//    subject.subscribe {
+//        print("Subscription Second: ", $0)
+//    }.disposed(by: disposableBag)
+//
+//    subject.onNext("c")
+//    subject.onNext("d")
+    
+    let subject = ReplaySubject<Int>.create(bufferSize: 3)
+    subject.onNext(1)
+    subject.onNext(2)
+    subject.onNext(3)
+    subject.onNext(4)
+    
+    subject.subscribe {
+            print( $0)
+        }.disposed(by: disposableBag)
+}
+
+example("Variables") {
+    let disposableBag = DisposeBag()
+    
+    let variable = Variable("A")
+    
+    variable.asObservable().subscribe(onNext:{
+        print($0)
+    }).disposed(by: disposableBag)
+    
+    variable.value = "B"
 }
