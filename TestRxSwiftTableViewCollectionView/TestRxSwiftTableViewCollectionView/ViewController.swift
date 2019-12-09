4ee867182901d3c4750756d0140221f5435c886d
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-09 11:17:36 +0200

    create tableView with data

--- TestRxSwiftTableViewCollectionView/TestRxSwiftTableViewCollectionView/ViewController.swift
+++ TestRxSwiftTableViewCollectionView/TestRxSwiftTableViewCollectionView/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    let foods = [
+        Food(name: "Fastfood", flickrID: "fastfood"),
+        Food(name: "Imageresizer", flickrID: "imageresizer"),
+        Food(name: "Images", flickrID: "images"),
+        Food(name: "Recipes-header", flickrID: "recipes-header-1600x1066")
+    ]
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,3 +31,27 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foods.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { return UITableViewCell() }
+        
+        let food = foods[indexPath.row]
+        cell.textLabel?.text = food.name
+        cell.detailTextLabel?.text = food.flickrID
+        cell.imageView?.image = food.image
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("your selcted: \(indexPath.row)")
+    }
+}
