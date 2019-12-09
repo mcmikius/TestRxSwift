af3b94a70a8a7271abc2ba50ec1dd37377608ad8
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-09 17:53:55 +0200

    Rx network init

---
+++ TestRxNetworking/TestRxNetworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRxNetworking
+//
+//  Created by Mykhailo Bondarenko on 09.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
